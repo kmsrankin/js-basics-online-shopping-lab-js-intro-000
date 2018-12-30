7507d671f77cd09406876aa79c0e7978f0d7add1
Keegan Rankin <kms.rankin@gmail.com>
2018-12-30 16:04:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -39,15 +39,12 @@ function total() {
 }
 
 function removeFromCart(item) {
-  // var itemInCart = false
   for (var i = 0; i < cart.length; i++) {
     if (cart[i].itemName === item) {
       cart.splice(i, 1)
+    } if (!cart[i].itemName) {
+      alert "That item is not in your cart."
     }
-    // if (cart[i].hasOwnProperty(item)) {
-    // var itemInCart = true;
-    // cart.splice(i, 1)
-    // }
   }
   return  cart;// write your code here
 }
