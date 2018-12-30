7eb18b28e2acb73d717dea12eff4eafd201d0bf4
Keegan Rankin <kms.rankin@gmail.com>
2018-12-30 15:43:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ function removeFromCart(item) {
   // for (var i = 0; i < cart.length; i++) {
     if (cart[itemName].hasOwnProperty(item)) {
     var itemInCart = true;
-    cart = (...cart.slice(0, i), ...cart.slice(i+1))
+    cart = ( ...cart.slice(0, i), ...cart.slice(i+1))
     }
   }
   return  cart;// write your code here
