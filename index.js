f2d8d1b11ad1a9a2506dcdc77b3146e91f97be9e
Keegan Rankin <kms.rankin@gmail.com>
2018-12-30 16:08:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ function removeFromCart(item) {
     if (cart[i].itemName === item) {
       cart.splice(i, 1)
     } if (!cart[i].itemName) {
-      return ("That item is not in your cart.")
+      return "That item is not in your cart."
     }
   }
   return  cart;// write your code here
