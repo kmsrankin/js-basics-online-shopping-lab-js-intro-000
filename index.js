960f21cd2f7682197f63f99772fea9dcc67af7d4
Keegan Rankin <kms.rankin@gmail.com>
2018-12-30 16:08:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -42,9 +42,7 @@ function removeFromCart(item) {
   for (var i = 0; i < cart.length; i++) {
     if (cart[i].itemName === item) {
       cart.splice(i, 1)
-    } if (!cart[i].itemName) {
-      return "That item is not in your cart."
-    }
+    } 
   }
   return  cart;// write your code here
 }
