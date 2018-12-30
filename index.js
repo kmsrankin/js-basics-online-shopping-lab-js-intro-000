2a44057db545a4c08112bda1a28a41b1e2054217
Keegan Rankin <kms.rankin@gmail.com>
2018-12-30 16:13:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ function removeFromCart(item) {
       return cart;
     }
   }
-  alert("That item is not in your cart.")
+  console.log("That item is not in your cart.")
   return  cart;// write your code here
 }
 
